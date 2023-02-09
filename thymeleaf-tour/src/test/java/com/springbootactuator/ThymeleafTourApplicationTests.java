1a0aa8df57881bee87e001602ea480425a4586dd
KatyaMa <71144178+KatyaMa@users.noreply.github.com>
2023-02-09 10:14:18 -0800

    thymeleaf

---
+++ thymeleaf-tour/src/test/java/com/springbootactuator/ThymeleafTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
